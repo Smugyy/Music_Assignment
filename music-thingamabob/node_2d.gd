b99414d0e7fe8fdb258cdfb34b56d1ef85819df7
Smugyy <c24492646@mytudublin.ie>
2024-12-08 10:47:20 +0000

    The finished project

--- music-thingamabob/node_2d.gd
+++ music-thingamabob/node_2d.gd
@@ -3,25 +3,184 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _on_button_button_down() -> void:
-	if $AudioStreamPlayer2D.playing:
-		$Button.text="play"
-		$AudioStreamPlayer2D.stop()
+	if $"Audio Clips/AudioStreamPlayer2D".playing:
+		$"Audio Clips/AudioStreamPlayer2D".stop()
 	else:
-		$Button.text="stop"
-		$AudioStreamPlayer2D.play()
+		$"Audio Clips/AudioStreamPlayer2D".play()
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _on_h_slider_value_changed(value: float) -> void:
-	$AudioStreamPlayer2D.pitch_scale = value
-	
-	
-	pass
-func _on_button_2_button_down() -> void:
-	if $AudioStreamPlayer2D2.playing:
-		$Button2.text="play"
-		$AudioStreamPlayer2D2.stop()
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D".stream.loop = true
 	else:
-		$Button2.text="stop"
-		$AudioStreamPlayer2D2.play()
+		$"Audio Clips/AudioStreamPlayer2D".stream.loop = false
+	pass # Replace with function body.
+
+func _on_button_2_button_down() -> void:
+	if $"Audio Clips/AudioStreamPlayer2D2".playing:
+		$"Audio Clips/AudioStreamPlayer2D2".stop()
+	else:
+		$"Audio Clips/AudioStreamPlayer2D2".play()
+	pass # Replace with function body.
+
+func _on_check_box_2_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D2".stream.loop = true
+	else:
+		$"Audio Clips/AudioStreamPlayer2D2".stream.loop = false
+	pass # Replace with function body.
+
+func _on_button_3_button_down() -> void:
+	if $"Audio Clips/AudioStreamPlayer2D3".playing:
+		$"Audio Clips/AudioStreamPlayer2D3".stop()
+	else:
+		$"Audio Clips/AudioStreamPlayer2D3".play()
+	pass # Replace with function body.
+
+func _on_check_box_3_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D3".stream.loop = true
+	else:
+		$"Audio Clips/AudioStreamPlayer2D3".stream.loop = false
+	pass # Replace with function body.
+
+func _on_button_4_button_down() -> void:
+	if $"Audio Clips/AudioStreamPlayer2D4".playing:
+		$"Audio Clips/AudioStreamPlayer2D4".stop()
+	else:
+		$"Audio Clips/AudioStreamPlayer2D4".play()
+	pass # Replace with function body.
+
+func _on_check_box_4_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D4".stream.loop = true
+	else:
+		$"Audio Clips/AudioStreamPlayer2D4".stream.loop = false
+	pass # Replace with function body.
+
+func _on_button_5_button_down() -> void:
+	if $"Audio Clips/AudioStreamPlayer2D5".playing:
+		$"Audio Clips/AudioStreamPlayer2D5".stop()
+	else:
+		$"Audio Clips/AudioStreamPlayer2D5".play()
+	pass # Replace with function body.
+
+func _on_check_box_5_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D5".stream.loop = true
+	else:
+		$"Audio Clips/AudioStreamPlayer2D5".stream.loop = false
+	pass # Replace with function body.
+
+func _on_button_6_button_down() -> void:
+	if $"Audio Clips/AudioStreamPlayer2D6".playing:
+		$"Audio Clips/AudioStreamPlayer2D6".stop()
+	else:
+		$"Audio Clips/AudioStreamPlayer2D6".play()
+	pass # Replace with function body.
+
+func _on_check_box_6_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D6".stream.loop = true
+	else:
+		$"Audio Clips/AudioStreamPlayer2D6".stream.loop = false
+	pass # Replace with function body.
+
+func _on_button_7_button_down() -> void:
+	if $"Audio Clips/AudioStreamPlayer2D7".playing:
+		$"Audio Clips/AudioStreamPlayer2D7".stop()
+	else:
+		$"Audio Clips/AudioStreamPlayer2D7".play()
+	pass # Replace with function body.
+
+func _on_check_box_7_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D7".stream.loop = true
+	else:
+		$"Audio Clips/AudioStreamPlayer2D7".stream.loop = false
+	pass # Replace with function body.
+
+func _on_button_8_button_down() -> void:
+	if $"Audio Clips/AudioStreamPlayer2D8".playing:
+		$"Audio Clips/AudioStreamPlayer2D8".stop()
+	else:
+		$"Audio Clips/AudioStreamPlayer2D8".play()
+	pass # Replace with function body.
+
+func _on_check_box_8_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D8".stream.loop = true
+	else:
+		$"Audio Clips/AudioStreamPlayer2D8".stream.loop = false
+	pass # Replace with function body.
+
+func _on_button_9_button_down() -> void:
+	if $"Audio Clips/AudioStreamPlayer2D9".playing:
+		$"Audio Clips/AudioStreamPlayer2D9".stop()
+	else:
+		$"Audio Clips/AudioStreamPlayer2D9".play()
+	pass # Replace with function body.
+
+func _on_check_box_9_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D9".stream.loop = true
+	else:
+		$"Audio Clips/AudioStreamPlayer2D9".stream.loop = false
+	pass # Replace with function body.
+
+func _on_button_10_button_down() -> void:
+	if $"Audio Clips/AudioStreamPlayer2D10".playing:
+		$"Audio Clips/AudioStreamPlayer2D10".stop()
+	else:
+		$"Audio Clips/AudioStreamPlayer2D10".play()
+	pass # Replace with function body.
+
+func _on_check_box_10_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D10".stream.loop = true
+	else:
+		$"Audio Clips/AudioStreamPlayer2D10".stream.loop = false
+	pass # Replace with function body.
+
+func _on_button_11_button_down() -> void:
+	if $"Audio Clips/AudioStreamPlayer2D11".playing:
+		$"Audio Clips/AudioStreamPlayer2D11".stop()
+	else:
+		$"Audio Clips/AudioStreamPlayer2D11".play()
+	pass # Replace with function body.
+
+
+func _on_check_box_11_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D11".stream.loop = true
+	else:
+		$"Audio Clips/AudioStreamPlayer2D11".stream.loop = false
+	pass # Replace with function body.
+
+func _on_button_12_button_down() -> void:
+	if $"Audio Clips/AudioStreamPlayer2D12".playing:
+		$"Audio Clips/AudioStreamPlayer2D12".stop()
+	else:
+		$"Audio Clips/AudioStreamPlayer2D12".play()
+	pass # Replace with function body.
+
+func _on_check_box_12_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D12".stream.loop = true
+	else:
+		$"Audio Clips/AudioStreamPlayer2D12".stream.loop = false
+	pass # Replace with function body.
+
+func _on_button_13_button_down() -> void:
+	if $"Audio Clips/AudioStreamPlayer2D13".playing:
+		$"Audio Clips/AudioStreamPlayer2D13".stop()
+	else:
+		$"Audio Clips/AudioStreamPlayer2D13".play()
+	pass # Replace with function body.
+
+func _on_check_box_13_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$"Audio Clips/AudioStreamPlayer2D13".stream.loop = true
+	else:
+		$"Audio Clips/AudioStreamPlayer2D13".stream.loop = false
 	pass # Replace with function body.
